398d96b90cef93a56c6dcad6475fc80cae4976e8
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-03 10:43:12 +0200

    improvement: Add comments to main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { tempMovieData, tempWatchedData, average } from "../App";
 
+/**
+ * The Main function returns a JSX element containing a ListBox and a WatchedBox component.
+ * @returns The Main component is returning a JSX element. The JSX element represents the main section
+ * of the application and includes two child components, ListBox and WatchedBox.
+ */
 export default function Main() {
   return (
     <main className="main">
@@ -10,6 +15,14 @@ export default function Main() {
   );
 }
 
+/**
+ * The ListBox function renders a box with a toggle button that controls the visibility of a MovieList
+ * component.
+ * @returns The ListBox component is returning a div element with the class name "box". Inside the div,
+ * there is a button element with the class name "btn-toggle". The button has an onClick event handler
+ * that toggles the value of isOpen1 using the setIsOpen1 function. The text content of the button is
+ * determined by the value of isOpen1 - if isOpen1 is true, the button displays "–
+ */
 function ListBox() {
   const [isOpen1, setIsOpen1] = useState(true);
 
@@ -26,6 +39,13 @@ function ListBox() {
   );
 }
 
+/**
+ * The MovieList function returns a list of movies rendered as a series of Movie components.
+ * @returns The MovieList component is returning an unordered list (ul) with a className of "list".
+ * Inside the ul, it is mapping over the movies array and rendering a Movie component for each movie in
+ * the array. The Movie component is passed the movie object as a prop and is given a unique key using
+ * the movie's imdbID.
+ */
 function MovieList() {
   const [movies, setMovies] = useState(tempMovieData);
 
@@ -38,6 +58,12 @@ function MovieList() {
   );
 }
 
+/**
+ * The Movie function renders a list item with an image, title, and year for a given movie.
+ * @returns The Movie component is returning a list item element (<li>) that contains an image, a
+ * heading, and a paragraph. The image source is set to the movie's poster URL, the heading displays
+ * the movie's title, and the paragraph displays the movie's year.
+ */
 function Movie({ movie }) {
   return (
     <li key={movie.imdbID}>
@@ -53,6 +79,13 @@ function Movie({ movie }) {
   );
 }
 
+/**
+ * The WatchedBox component renders a box that can be toggled open or closed, displaying a summary and
+ * a list of watched movies.
+ * @returns The WatchedBox component is returning a div with the class name "box". Inside the div,
+ * there is a button with the class name "btn-toggle" that toggles the value of isOpen2 when clicked.
+ * The button text is "-" when isOpen2 is true and "+" when isOpen2 is false.
+ */
 function WatchedBox() {
   const [watched, setWatched] = useState(tempWatchedData);
   const [isOpen2, setIsOpen2] = useState(true);
@@ -75,6 +108,13 @@ function WatchedBox() {
   );
 }
 
+/**
+ * The WatchedSummary function calculates and displays the average IMDb rating, user rating, and
+ * runtime of a list of watched movies.
+ * @returns The WatchedSummary component is returning a JSX element that displays a summary of movies
+ * watched. The summary includes the number of movies watched, the average IMDb rating, the average
+ * user rating, and the average runtime of the movies.
+ */
 function WatchedSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
@@ -105,6 +145,12 @@ function WatchedSummary({ watched }) {
   );
 }
 
+/**
+ * The WatchedMovieList function renders a list of watched movies.
+ * @returns The WatchedMovieList component is returning an unordered list (ul) with a className of
+ * "list". Inside the ul, it is mapping over the "watched" array and rendering a WatchedMovie component
+ * for each movie in the array. The key prop is set to the movie's imdbID.
+ */
 function WatchedMovieList({ watched }) {
   return (
     <ul className="list">
@@ -115,6 +161,12 @@ function WatchedMovieList({ watched }) {
   );
 }
 
+/**
+ * The WatchedMovie function renders a list item with movie details, including the movie poster, title,
+ * IMDb rating, user rating, and runtime.
+ * @returns The WatchedMovie component is returning a list item element (<li>) with the following
+ * content:
+ */
 function WatchedMovie({ movie }) {
   return (
     <li key={movie.imdbID}>
