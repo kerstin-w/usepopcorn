b045632da54a325e8130ef76f32f72ba72cffc29
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-06 21:02:32 +0200

    docs: Add comment to Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,3 +1,8 @@
+/**
+ * The Loader function returns a paragraph element with the text "Loading..." and a class name of
+ * "loader".
+ * @returns A paragraph element with the class name "loader" and the text "Loading...".
+ */
 export function Loader() {
   return <p className="loader">Loading...</p>;
 }
