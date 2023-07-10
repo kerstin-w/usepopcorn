a28db24038fabfd44900b47f9f36cdb053230e78
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-10 11:02:06 +0200

    feat: Add effect to change page title

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -99,6 +99,14 @@ export function MovieDetails({
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
