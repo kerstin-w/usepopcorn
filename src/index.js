0b361b27713be0741496044bf8fcd9a2ebdb4750
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-10 10:51:33 +0200

    refactor: Remove StarRating from index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import StarRating from "./components/StarRating";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <StarRating
-      maxRating={5}
-      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    />
   </React.StrictMode>
 );
