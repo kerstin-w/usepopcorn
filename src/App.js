31e61181e44f86ba04cfee5aaae9b4120f6dd158
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-10 11:24:14 +0200

    improvement: Add cleanup function for fetching movies on search

--- src/App.js
+++ src/App.js
@@ -84,12 +84,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) throw new Error("Something went wrong");
@@ -99,9 +102,13 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found 🤷‍♀️");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -113,6 +120,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
